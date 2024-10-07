2a67bf25f06ac6ceb6e5cd5658fd6d9825191660
your name <you@example.om>
2024-10-07 12:44:53 +0300

    create app class and file and connect it with main

---
+++ gui/app.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title()
+        self.geometry()
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from gui.app import App
+
+
+app = App()
+app.mainloop()
