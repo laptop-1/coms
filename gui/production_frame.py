dacd15f9d435303b9d20cd8e648178eabd39ee27
your name <you@example.om>
2024-10-09 14:10:27 +0300

    grid and paritition the production frame

---
+++ gui/production_frame.py
@@ -0,0 +1,19 @@
+import customtkinter as ctk
+
+class ProductionFrame(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+        #frame positioning
+        self.main_frame_griding()
+        #frame structuring
+        self.main_frame_partitioning()
+
+    def main_frame_griding(self):
+        self.grid(column=0, row=0, sticky="nsew")
+
+    def main_frame_partitioning(self):
+        self.rowconfigure(0, weight=1, uniform='a')
+        self.rowconfigure((1,2), weight=2, uniform='a')
+        self.columnconfigure(0, weight=1,uniform='a')
+        self.columnconfigure(1, weight=2, uniform='a')
+        self.columnconfigure(2, weight=1, uniform='a')
