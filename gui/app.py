0761475e30df1adbe870e51d2e4dfafe76f263bd
your name <you@example.om>
2024-10-09 12:34:44 +0300

    make screen fullscreen

--- gui/app.py
+++ gui/app.py
@@ -4,5 +4,13 @@ class App(ctk.CTk):
     def __init__(self, title, dimensions):
         super().__init__()
         self.title(title)
-        self.geometry(f"{dimensions[0]}x{dimensions[1]}")
+        self.full_screen()
+
+        #window structure
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+    def full_screen(self):
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
+
 
---
+++ gui/main_frame.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+
+class MainFrame(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+
+        #frame positioning
+        self.grid()
+
+        #frame structuring
+        self.rowconfigure
+        self.columnconfigure
